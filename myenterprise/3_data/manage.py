e7254752c83131424db5de2aede631de30761d28
neranjana <neranjana@gmail.com>
2025-10-08 13:51:15 +1100

    Delete manage.py and add some building blocks

--- myenterprise/3_data/manage.py
+++
@@ -1,8 +0,0 @@
-# This will run on the terminal and ask the suer something like
-# What do you want to do? Enter
-# 1 to create a subfolder
-# 2 to create a building block in this folder
-# 3 to create a diagram in this folder
-# Then it will prompt the user to specify the type of the diagram or building block
-# If it was a subfolder, then this program will create a subfolder with a manage.py file in it.
-# If it was a building block or diagram, this program will create a python file for that.
